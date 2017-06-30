f0270e7265960895d7ae1697553a878d58b057b1
earl-jann <earljanntormes@gmail.com>
2017-06-30 17:23:11 +0800

    Redmine#88 - Kurento Room Launcher   - npm based launcher

--- kurento/scripts/docker/docker-run-kurento-room.sh
+++ kurento/scripts/docker/docker-run-kurento-room.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-docker run --rm --net=host --network=kurento-network -it -p 8080:8080 -p 8443:8443 -v /home/earljann/data/kurento-room:/usr/local/lohika/data --name=kurento-room-01 274425231303.dkr.ecr.us-east-1.amazonaws.com/lohika /usr/local/lohika/data/start.sh
+docker run --rm --net=host --network=kurento-network -it -p 8080:8080 -p 8443:8443 -v /home/earljann/data/kurento-room:/usr/local/lohika/data --name=kurento-room-01 274425231303.dkr.ecr.us-east-1.amazonaws.com/lohika npm run --prefix /usr/local/lohika/data launch-app
